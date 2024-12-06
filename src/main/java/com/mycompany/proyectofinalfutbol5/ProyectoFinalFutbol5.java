7fb80b622ae632ce7f8f29439e3df8e7927c42aa
LuisMora204 <luisemanuelmm@gmail.com>
2024-12-05 19:06:07 -0600

    hola

--- src/main/java/com/mycompany/proyectofinalfutbol5/ProyectoFinalFutbol5.java
+++ src/main/java/com/mycompany/proyectofinalfutbol5/ProyectoFinalFutbol5.java
@@ -59,10 +59,6 @@ public class ProyectoFinalFutbol5 {
 //Para este segundo avance (5% del total del proyecto), se solicita presentar: 
 // Haber implementado los constructores y el encapsulamiento en las clases del proyecto.
 // El sistema debería contener arreglos de objetos para manipular los objetos dentro del sistema.
-// Implementación de la relación entre las clases, donde los jugadores se asocian a un equipo y 
-//viceversa. 
 // La gestión completa de jugadores y equipos en el sistema, con la funcionalidad de agregar, editar y 
-//eliminar. 
-// Una simulación básica de partidos, aunque no sea completa, que muestre el uso de los jugadores y 
-//equipos en el sistema. 
+//eliminar.
 
